0f26c7ce4c582940161ac93ebf3b21e8b99eb2ed
trololo <solembum@sole.com>
2020-06-25 20:45:33 +0300

    add index.blade.php in views make default.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+    @section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title">Приветсвуем Вас <br> на нашем сайте новостей</div>
+            <a href="" label="news">Показать все новости</a>
+        </div>
+    </div>
+        @endsection;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,14 +31,15 @@
             }
 
             .title {
-                font-size: 96px;
+                font-size: 50px;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Приветсвуем Вас <br> на нашем сайте новостей</div>
+                <a href="" label="news">Показать все новости</a>
             </div>
         </div>
     </body>
