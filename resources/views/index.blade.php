dee0e3b127de60d8f61c72bab19c26aa9bc6af4a
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2020-06-30 15:24:35 +0300

    changes in index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.default')
-    @section('content')
+@section('content')
     <div class="container">
         <div class="content">
             <div class="title">Приветсвуем Вас <br> на нашем сайте новостей</div>
-            <a href="" label="news">Показать все новости</a>
+            <button type="button" class="btn btn-info"><a href="news.list" label="news">Показать все новости</a></button>
         </div>
     </div>
-        @endsection;
+@endsection;
