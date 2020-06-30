f29078435d178f0c29473b74dbcac2648c18b064
Dmytro_Yermolaiev <iermolaiev1@gmail.com>
2020-06-30 15:22:55 +0300

    add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,28 @@
 |
 */
 
+use App\News;
+
 Route::get('/', function () {
     return view('index');
 });
+
+Route::group(['prefix' => 'news'], function () {
+
+    /**
+     * Вывести панель с новостями
+     */
+    Route::get('/', function () {
+        $news = \App\News::all();
+        return view('news.list', ['news' => $news]);
+    })->name('news.list');
+
+    /**
+     * Добавить новую задачу
+     */
+    Route::get('/create', function () {
+        return view('news.create');
+    })->name('news.create');
+
+});
+
