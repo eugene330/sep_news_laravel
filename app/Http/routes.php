f9ed99b8bbb1c1dfb4a6166251490476ccdb46ee
trololo <solembum@sole.com>
2020-06-30 19:57:21 +0300

    create Route:store + Route:show

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,4 +34,25 @@ Route::group(['prefix' => 'news'], function () {
         return view('news.create');
     })->name('news.create');
 
+    Route::post('/', function (\Illuminate\Http\Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+        ]);
+        if ($validator->fails()) {
+            return redirect(route('news.create'))
+                ->withInput()
+                ->withErrors($validator);
+        }
+        \App\News::create([
+            'title' => $request->title,
+            'text' => $request->text
+        ]);
+        return redirect(route('news.list'));
+    })->name('news.store');
+
+    Route::get('/{news}', function (News $news) {
+        return view('news.show', ['news' => $news]);
+    })->name('news.show');
+
+
 });
