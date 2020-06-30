3b69fab6bf619b1da68ebcbdbc268e65eec9d911
trololo <solembum@sole.com>
2020-06-30 19:59:13 +0300

    final changes in default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,42 +1,40 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>News</title>
+    <head>
+        <title>News</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <style>
+            html, body {
+                height: 100%;
+                background-color: #cecece;
+            }
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: Lato, serif;
+            }
 
-    <style>
-        html, body {
-            height: 100%;
-            background-color: #cecece;
-        }
+            .container {
+                text-align: center;
+                vertical-align: middle;
+            }
 
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
 
-        .container {
-            text-align: center;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 50px;
-        }
-    </style>
-</head>
-<body>
-@yield('content')
-</body>
+            .title {
+                font-size: 50px;
+            }
+        </style>
+    </head>
+    <body>
+        @yield('content')
+    </body>
 </html>
